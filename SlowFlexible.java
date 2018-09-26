30bbbb8c5161f8ca3e24e245a63ed248c3bf7f45
AmousQiu <571459556@qq.com>
2018-09-26 11:38:47 -0300

    Update SlowFlexible.java

--- SlowFlexible.java
+++ SlowFlexible.java
@@ -2,5 +2,6 @@ public class SlowFlexible extends SlowPiece {
     SlowFlexible(String name, String colour, int[][] position) {
         super(name, colour, position);
         //hello
+        //nice to meet you
     }
 }
